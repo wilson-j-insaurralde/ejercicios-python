7bb54c87b40541bb365c9122514cee36b1e5a29d
wilson-j-insaurralde <wilson-insaurralde@hotmail.com>
2023-06-28 15:43:41 -0300

    18

--- ejercicio155.py
+++ ejercicio155.py
@@ -5,5 +5,29 @@
 #1) Función para cargar todos los candidatos, sus nombres y las provincias con los votos obtenidos.
 #2) Imprimir el nombre del candidato y la cantidad total de votos obtenidos en todas las provincias.
 
-def candidatos():
-    
+def cargar_candidatos():
+    candidatos=[]
+    for x in range(3):
+        nom=input("Ingrese el nombre del candidato:")
+        cant=int(input("Los votos de cuantas provincias tiene para cargar?"))
+        provincias=[]
+        for z in range(cant):
+            prov=input("Nombre de provincia:")
+            votos=int(input("Cantidad de votos:"))
+            provincias.append((prov,votos))
+        candidatos.append((nom,provincias))
+    return candidatos
+
+
+def totalvotos_candidato(candidatos):
+    for x in range(len(candidatos)):
+        suma=0
+        for z in range(len(candidatos[x][1])):
+            suma=suma+candidatos[x][1][z][1]
+        print(candidatos[x][0],suma)    
+        
+
+# bloque principal
+
+candidatos=cargar_candidatos()
+totalvotos_candidato(candidatos)
